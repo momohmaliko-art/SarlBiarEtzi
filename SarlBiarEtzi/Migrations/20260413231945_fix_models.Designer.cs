4ce8e265a01bcec3b3ccd896d1dcff2df53c65e5
Abdelmalek <momohmaliko@gmail.com>
2026-04-14 01:31:39 +0200

    fix database connection

--- SarlBiarEtzi/Migrations/20260413231945_fix_models.Designer.cs
+++ SarlBiarEtzi/Migrations/20260413231945_fix_models.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SarlBiarEtzi.Models;
@@ -11,9 +12,11 @@ using SarlBiarEtzi.Models;
 namespace SarlBiarEtzi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260413231945_fix_models")]
+    partial class fix_models
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
